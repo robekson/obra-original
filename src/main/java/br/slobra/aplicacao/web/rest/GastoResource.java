401f4b4a1a5e5136fb54c68b2f3911ea335229b9
811198 <811198@D-PROD-BP137475.prodesp-sede.local>
2019-01-22 11:06:46 -0200

    ajustes

--- src/main/java/br/slobra/aplicacao/web/rest/GastoResource.java
+++ src/main/java/br/slobra/aplicacao/web/rest/GastoResource.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
+import br.slobra.aplicacao.domain.enumeration.NotaFiscal.*;
 
 import javax.validation.Valid;
 import java.net.URI;
