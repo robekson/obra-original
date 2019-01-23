6328303b12fa1c916dcdb59ad617279e6bf9d620
811198 <811198@D-PROD-BP137475.prodesp-sede.local>
2019-01-23 17:36:55 -0200

    ajustes

--- src/main/webapp/app/entities/gasto/gasto.service.ts
+++ src/main/webapp/app/entities/gasto/gasto.service.ts
@@ -60,25 +60,6 @@ export class GastoService {
             .pipe(map((res: EntityResponseTypeResumo) => this.convertDateArrayFromServer2(res)));
     }
     
-    
-    private extractData(res: Response) {
-        let body = res.json();
-        return body || { };
-   }
-    
-    private handleError (error: Response | any) {
-        // In a real world app, you might use a remote logging infrastructure
-        let errMsg: string;
-        if (error instanceof Response) {
-          const body = error.json() || '';
-          const err =  JSON.stringify(body);
-          errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-        } else {
-          errMsg = error.message ? error.message : error.toString();
-        }
-        console.error(errMsg);
-        return throwError(errMsg);
-    }
 
     delete(id: number): Observable<HttpResponse<any>> {
         return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response' });
