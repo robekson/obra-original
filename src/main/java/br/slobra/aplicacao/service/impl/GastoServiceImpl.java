0f44d6139d0585cb5c350c82a1009ddb3b29ada1
811198 <811198@D-PROD-BP137475.prodesp-sede.local>
2019-01-28 18:24:49 -0200

    ajustes

--- src/main/java/br/slobra/aplicacao/service/impl/GastoServiceImpl.java
+++ src/main/java/br/slobra/aplicacao/service/impl/GastoServiceImpl.java
@@ -68,6 +68,7 @@ public class GastoServiceImpl implements GastoService {
     @Transactional(readOnly = true)
     public List<GastoDTO> findByObra(Long idObra) {
         log.debug("Request to get all Gastos");
+        
         return gastoRepository.findByObra(idObra).stream().map(gastoMapper::toDto).collect(Collectors.toList());
     }
 
