d872f6a034940e78e968cae87238c79b125c0371
811198 <811198@D-PROD-BP137475.prodesp-sede.local>
2019-02-04 16:14:48 -0200

    ajustes root

--- src/main/webapp/app/dashboard/linechart/linechart.route.ts
+++ src/main/webapp/app/dashboard/linechart/linechart.route.ts
@@ -3,7 +3,8 @@ import { UserRouteAccessService } from '../../core';
 import { LinechartComponent } from './linechart.component';
 
 export const linechartRoute: Route = {
-    path: 'linechart',
+   // path: 'linechart',
+    path: '',
     component: LinechartComponent,
     data: {
         authorities: ['ROLE_USER'],
