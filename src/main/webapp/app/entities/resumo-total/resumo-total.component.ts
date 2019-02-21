4c880def22e33bd4707de6c738b8fa71e42903a9
Robekson <robeksonrm@gmail.com>
2019-02-20 23:59:15 -0300

    ajustes

--- src/main/webapp/app/entities/resumo-total/resumo-total.component.ts
+++ src/main/webapp/app/entities/resumo-total/resumo-total.component.ts
@@ -85,7 +85,7 @@ export class ResumoTotalComponent implements OnInit, OnDestroy {
         console.log('exportar =' + data);
         html2canvas(data).then(canvas => {
             // Few necessary setting options
-            var imgWidth = 195;
+            var imgWidth = 190;
             var pageHeight = 695;
             var imgHeight = (canvas.height * imgWidth) / canvas.width;
             var heightLeft = imgHeight;
