f8db80439a2dc5b7b45579695416026f549e3c99
811198 <811198@D-PROD-BP137475.prodesp-sede.local>
2019-11-18 16:49:45 -0300

    ajustes ngx uilodardeeer

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
 
 import { JhiLanguageHelper } from 'app/core';
+import { NgxUiLoaderService } from 'ngx-ui-loader'; 
 
 @Component({
     selector: 'jhi-main',
     templateUrl: './main.component.html'
 })
 export class JhiMainComponent implements OnInit {
-    constructor(private jhiLanguageHelper: JhiLanguageHelper, private router: Router) {}
+    constructor(private jhiLanguageHelper: JhiLanguageHelper, private router: Router,
+            private ngxService: NgxUiLoaderService) {}
 
     private getPageTitle(routeSnapshot: ActivatedRouteSnapshot) {
         let title: string = routeSnapshot.data && routeSnapshot.data['pageTitle'] ? routeSnapshot.data['pageTitle'] : 'obrasApp';
@@ -19,10 +21,12 @@ export class JhiMainComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.ngxService.start(); 
         this.router.events.subscribe(event => {
             if (event instanceof NavigationEnd) {
                 this.jhiLanguageHelper.updateTitle(this.getPageTitle(this.router.routerState.snapshot.root));
             }
         });
+        this.ngxService.stop();
     }
 }
